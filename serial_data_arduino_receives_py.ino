001101c52279142bce4e0d357c10ad21900d020c
Arafat Mukasa <araphatarafat@gmail.com>
2019-04-10 12:32:52 +0300

    Update serial_data_arduino_receives_py.ino
    This is the respective code for turning on/off and led using voice. This code is uploaded onto a arduino. I used arduino uno but other arduino boards like mega should work. The led is connected to pin number 7 of arduino uno. Please note that i had an extrnal led connected to use pin 7. If you do not have the external circuit (comprising of a led and a resistor and jumper wires), then instead of pin 7, use pin 13 of arduino uno.

--- serial_data_arduino_receives_py.ino
+++ serial_data_arduino_receives_py.ino
@@ -1,14 +1,48 @@
-void setup () {
+const int ledPin = 7;
+
+char data;
+
+
+void setup () 
+{
   Serial.begin (115200);
+  pinMode (ledPin, OUTPUT);
+
+}  
+
   
+int getData () {
+  
+  if (Serial.available ()) 
+  {
+     data = Serial.read ();
+  }  
+
+  return data;
 }
 
-void loop () {
-  if (Serial.available () > 0 ) {
-    char data = Serial.read ();
-    Serial.print("from Python, i have received: ");
-    Serial.println ((data));
-
+void switch_on (char input){
+  
+  if (input == '1') {
+    digitalWrite (ledPin, HIGH);
   }
 }
 
+
+void switch_off (char input) {
+
+  if (input == '0') {
+    
+    digitalWrite (ledPin, LOW);
+  }
+}
+
+void loop () 
+{ 
+  getData();
+
+  switch_on (getData ());
+
+  switch_off (getData ());
+
+}
